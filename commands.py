0acde780d6b15403429d73b39ca9b6d26216d619
Bellavene <77557804+Bellavene@users.noreply.github.com>
2022-07-11 06:12:23 +0300

    Update commands.py

--- commands.py
+++ commands.py
@@ -56,7 +56,7 @@ class follow_files(Command):
         if follow:
             self.fm.execute_console("shell -s open -R %s")
             
-            # Updates Window Manager Layout CMD+OPT+CTRL+SHIFT+R / Change to yours 
+            # Updates Window Manager Layout CMD+OPT+CTRL+SHIFT+R / Change to yours or delete entirely if not needed
             self.fm.execute_console("shell -s osascript -e 'tell application \"System Events\" to keystroke \"r\" using control down, command down, option down, shift down'")
 
             # Sets Finder window to Gallery View via standard key CMD+4
