2458cdb7fb5542ca9fa066afbb7bc7b801d0dda4
Bellavene <77557804+Bellavene@users.noreply.github.com>
2023-11-12 01:01:43 +0300

    Update commands.py
    Rewritten with use of shell scripts

--- commands.py
+++ commands.py
@@ -10,6 +10,32 @@ class show_files_in_finder(Command):
         self.fm.notify(script)
         subprocess.check_output(["osascript", "-e", reveal_script, "-e", activate_script])
 
+class toggle_fplug(Command):
+
+    def execute(self):
+        p = not self.fm.settings["_fplug"]
+        self.fm.settings["_fplug"] = p
+        self.fm.execute_console(f"fplug {'%s' if p else ''}")
+        status = f"fplug {'active' if p else 'off'}"
+
+class fplug(Command):
+
+    def execute(self):
+        from pathlib import Path
+        p = Path('{f}')
+        do_preview = self.fm.settings["_fplug"]
+        try:
+            f = self.rest(1)
+            if not do_preview:
+                self.fm.execute_console(f"shell -s osascript -e 'tell application \"Finder\" to close its front window'")
+            elif not termplug:
+                self.fm.notify(f"fplug script not found")
+            elif os.path.isfile(f):
+                self.fm.execute_console(f"shell -s finder-set-front '{f}'")
+            elif os.path.isdir(f):
+                self.fm.execute_console(f"shell -s finder-set-front '{f}'")
+        except Exception as e:
+            self.fm.notify(e)
 
 # Reveal file from Finder
 class get_finder(Command):
@@ -27,61 +53,6 @@ class get_finder(Command):
                 self.fm.cd(selected)
             else:
                 self.fm.select_file(selected)
-
-
-# Quicklook / Follow files in Finder
-class follow_files_in_finder(Command):
-
-    def execute(self):
-        import subprocess
-        file = '"{0}"'.format(self.fm.thisfile.path)
-        script1 = "set thePath to {{{0}}} as text".format(file)
-        script2 = "set name_ to name of (info for thePath)"
-        script3 = "set a_ to count thePath"
-        script4 = "set b_ to count name_"
-        script5 = "set minus_ to a_ - b_"
-        script6 = "set parentPath to text 1 thru (minus_) of thePath"
-        script7 = "tell application \"Finder\" to set target of front Finder window to (POSIX file parentPath)"
-        script8 = "tell application \"Finder\" to reveal thePath as POSIX file"
-        script9 = "tell application \"Finder\" to set frontmost to false"
-        script = "osascript -e '{0}' -e '{1}' -e '{2}' -e '{3}' -e '{4}' -e '{5}' -e '{6}' -e '{7}' -e '{8}'".format(script1, script2, script3, script4, script5, script6, script7, script8, script9)
-        subprocess.check_output(["osascript", "-e", script1, "-e", script2, "-e", script3, "-e", script4, "-e", script5, "-e", script6, "-e", script7, "-e", script8, "-e", script9])
-
-follow = False
-class follow_files(Command):
-
-    def execute(self):
-        global follow
-        follow = not follow
-        if follow:
-            # Updates Window Manager Layout CMD+OPT+CTRL+SHIFT+R / Uncomment if needed.
-            # self.fm.execute_console("shell -s osascript -e 'tell application \"System Events\" to keystroke \"r\" using {control down, command down, option down, shift down}'")
-
-            # Sets Finder window to Gallery View and moves focus back to terminal, in this case to iTerm
-            self.fm.execute_console("shell -s open -R %s; sleep 0.2; osascript -e 'tell application \"System Events\"' -e 'Click menu item \"as Gallery\" of menu 1 of menu bar item \"View\" of menu bar 1 of application process \"Finder\"' -e 'end tell'; osascript -e 'tell application \"iTerm\" to activate'")
-            
-            # Change to your directional keys
-            self.fm.execute_console("map <UP>       chain move up=1;    follow_files_in_finder")
-            self.fm.execute_console("map <DOWN>     chain move down=1;  follow_files_in_finder")
-            self.fm.execute_console("map <LEFT>     chain move left=1;  follow_files_in_finder")
-            self.fm.execute_console("map <RIGHT>    chain move right=1; follow_files_in_finder")
-            self.fm.execute_console("map <HOME>     chain move to=0;    follow_files_in_finder")
-            self.fm.execute_console("map <END>      chain move to=-1;   follow_files_in_finder")
-            self.fm.execute_console("map <PAGEDOWN> chain move down=1   pages=True; follow_files_in_finder")
-            self.fm.execute_console("map <PAGEUP>   chain move up=1     pages=True; follow_files_in_finder")
-        else:
-            # Revert Finder's view mode and closes it's Window / Change to desired view mode if needed
-            self.fm.execute_console("shell -s osascript -e 'tell application \"Finder\" to set the current view of the front Finder window to list view' -e 'tell application \"Finder\" to close its front window'")
-            
-            # Reverts key maps on stop file following
-            self.fm.execute_console("map <UP>       move up=1")
-            self.fm.execute_console("map <DOWN>     move down=1")
-            self.fm.execute_console("map <LEFT>     move left=1")
-            self.fm.execute_console("map <RIGHT>    move right=1")
-            self.fm.execute_console("map <HOME>     move to=0")
-            self.fm.execute_console("map <END>      move to=-1")
-            self.fm.execute_console("map <PAGEDOWN> move down=1   pages=True")
-            self.fm.execute_console("map <PAGEUP>   move up=1     pages=True")
 
 # Toggle Finder's red and ranger's standard tags on selected files
 class red_tag(Command):
