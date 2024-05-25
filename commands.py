42b70f86bcc27c4e62d7737e9710dddd16501317
Bellavene <77557804+Bellavene@users.noreply.github.com>
2024-05-26 02:59:25 +0300

    1.0

--- commands.py
+++ commands.py
@@ -67,6 +67,16 @@ class finder_get(Command):
             else:
                 self.fm.select_file(selected)
 
+class finder_info(Command):
+    def execute(self):
+        import subprocess
+        files = ",".join(['"{0}" as POSIX file'.format(file.path) for file in self.fm.thistab.get_selection()])
+        reveal_script = "set aFile to {{{0}}} as alias".format(files)
+        activate_script = "tell application \"Finder\" to open information window of aFile"
+        script = "osascript -e '{0}' -e '{1}'".format(reveal_script, activate_script)
+        self.fm.notify(script)
+        subprocess.check_output(["osascript", "-e", reveal_script, "-e", activate_script])
+
 # Opens quicklook in fullscreen on active file
 class quicklook(Command):
     def execute(self):
